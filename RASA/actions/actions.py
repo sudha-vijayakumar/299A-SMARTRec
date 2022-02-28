7496cf031b5d386c0d662042f10c350e20cc2658
Sudha Vijayakumar <69941575+sudha-vijayakumar@users.noreply.github.com>
2022-02-28 01:20:35 -0800

    conceptnet

--- RASA/actions/actions.py
+++ RASA/actions/actions.py
@@ -40,27 +40,27 @@ from pyspark.ml.recommendation import ALSModel
 from py2neo import Graph
 
 # sentence embedding selection.
-sentence_transformer_select=True
-pretrained_model='stsb-roberta-large' 
-score_threshold = 0.60  # This confidence scores can be adjusted based on your need!!
+# sentence_transformer_select=True
+# pretrained_model='stsb-roberta-large' 
+# score_threshold = 0.60  # This confidence scores can be adjusted based on your need!!
 topK=5
 
 # Load embedded features.
-root = '/Users/sudhavijayakumar/Documents/299/299A-SMARTRec/'
+root = '/Users/sudhavijayakumar/Desktop/299/299A-SMARTRec'
 embeddings = root+'/RASA/data/'
 raw = root+'/Data/raw/'
-list_embedding_model = Doc2Vec.load(embeddings+'embeddings/list_embeddings')
-review_embedding_model = Doc2Vec.load(embeddings+'embeddings/review_embeddings')
-offline_models=root+'RASA/offline_models/'
+# list_embedding_model = Doc2Vec.load(embeddings+'embeddings/list_embeddings')
+# review_embedding_model = Doc2Vec.load(embeddings+'embeddings/review_embeddings')
+# offline_models=root+'RASA/offline_models/'
 
 # Load dataset to get listings and reviews.
 listings = pd.read_csv(raw+'listings.csv.gz', sep=',', usecols = ['id','listing_url','picture_url','name','description','neighbourhood','property_type','bedrooms','bathrooms','amenities','price','review_scores_rating']) 
 reviews = pd.read_csv(raw+'reviews.csv.gz', sep=',', usecols = ['listing_id','comments']) 
 
 # use model to find the listings
-spark = SparkSession.builder.getOrCreate()
-sc = spark.sparkContext
-alsmodel = ALSModel.load(offline_models+"als_model")
+# spark = SparkSession.builder.getOrCreate()
+# sc = spark.sparkContext
+# alsmodel = ALSModel.load(offline_models+"als_model")
 
 USE_NEO4J = bool(os.getenv("USE_NEO4J", True))
 
@@ -102,93 +102,93 @@ def realTimeRecommendation(topK):
 
 neighbours_ids = realTimeRecommendation(5)
 
-# Recommendations based on feature embeddings.
-class ActionlistingsDetails_Embedding(Action):
+# # Recommendations based on feature embeddings.
+# class ActionlistingsDetails_Embedding(Action):
 
-	def name(self) -> Text:
-		return "action_listings_details_embedding"
+# 	def name(self) -> Text:
+# 		return "action_listings_details_embedding"
 
-	def run(self, dispatcher: CollectingDispatcher,
-			tracker: Tracker,
-			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+# 	def run(self, dispatcher: CollectingDispatcher,
+# 			tracker: Tracker,
+# 			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-		userMessage = tracker.latest_message['text']
-		# use model to find the listings
-		new_doc = preprocess_string(userMessage)
-		test_doc_vector = list_embedding_model.infer_vector(new_doc)
-		sims = list_embedding_model.docvecs.most_similar(positive = [test_doc_vector])		
+# 		userMessage = tracker.latest_message['text']
+# 		# use model to find the listings
+# 		new_doc = preprocess_string(userMessage)
+# 		test_doc_vector = list_embedding_model.infer_vector(new_doc)
+# 		sims = list_embedding_model.docvecs.most_similar(positive = [test_doc_vector])		
 		
-		# Get first 5 matches
-		for s in sims[:1]:
-			picture = listings['picture_url'].iloc[s[0]]
-			listingss = listings['listing_url'].iloc[s[0]]
-			name = listings['name'].iloc[s[0]]
-			description = listings['description'].iloc[s[0]]
-			neighbourhood = listings['neighbourhood'].iloc[s[0]]
-			bedroom = listings['bedrooms'].iloc[s[0]]
-			bathroom = listings['bathrooms'].iloc[s[0]]
-			amenities = listings['amenities'].iloc[s[0]]
-			price = listings['price'].iloc[s[0]]
-			review_score_rating = listings['review_scores_rating'].iloc[s[0]]
+# 		# Get first 5 matches
+# 		for s in sims[:1]:
+# 			picture = listings['picture_url'].iloc[s[0]]
+# 			listingss = listings['listing_url'].iloc[s[0]]
+# 			name = listings['name'].iloc[s[0]]
+# 			description = listings['description'].iloc[s[0]]
+# 			neighbourhood = listings['neighbourhood'].iloc[s[0]]
+# 			bedroom = listings['bedrooms'].iloc[s[0]]
+# 			bathroom = listings['bathrooms'].iloc[s[0]]
+# 			amenities = listings['amenities'].iloc[s[0]]
+# 			price = listings['price'].iloc[s[0]]
+# 			review_score_rating = listings['review_scores_rating'].iloc[s[0]]
 
-		botResponse = "Please find the top listing details:\nlink: "+str(listingss)+"\nTitle: "+str(name)+"\nDescription: "+str(description)+"\nNeighbourhood: "+str(neighbourhood)+"\nBedroom: "+str(bedroom)+"\nBathroom: "+str(bathroom)+"\nAmenities: "+str(amenities)+"\nPrice: "+str(price)+" per night\nRating: "+str(review_score_rating)+" on a scale of 5"
+# 		botResponse = "Please find the top listing details:\nlink: "+str(listingss)+"\nTitle: "+str(name)+"\nDescription: "+str(description)+"\nNeighbourhood: "+str(neighbourhood)+"\nBedroom: "+str(bedroom)+"\nBathroom: "+str(bathroom)+"\nAmenities: "+str(amenities)+"\nPrice: "+str(price)+" per night\nRating: "+str(review_score_rating)+" on a scale of 5"
 		
-		dispatcher.utter_message(text=botResponse)
-		dispatcher.utter_message(image=picture)
+# 		dispatcher.utter_message(text=botResponse)
+# 		dispatcher.utter_message(image=picture)
 
-		return []
+# 		return []
 
-class ActionlistingsSearch_Embedding(Action):
+# class ActionlistingsSearch_Embedding(Action):
 
-	def name(self) -> Text:
-		return "action_listings_search_embedding"
+# 	def name(self) -> Text:
+# 		return "action_listings_search_embedding"
 
-	def run(self, dispatcher: CollectingDispatcher,
-			tracker: Tracker,
-			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+# 	def run(self, dispatcher: CollectingDispatcher,
+# 			tracker: Tracker,
+# 			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-		userMessage = tracker.latest_message['text']
-		# use model to find the listings
-		new_doc = preprocess_string(userMessage)
-		test_doc_vector = list_embedding_model.infer_vector(new_doc)
-		sims = list_embedding_model.docvecs.most_similar(positive = [test_doc_vector])		
+# 		userMessage = tracker.latest_message['text']
+# 		# use model to find the listings
+# 		new_doc = preprocess_string(userMessage)
+# 		test_doc_vector = list_embedding_model.infer_vector(new_doc)
+# 		sims = list_embedding_model.docvecs.most_similar(positive = [test_doc_vector])		
 		
-		# Get first 5 matches
-		listingss = [listings['listing_url'].iloc[s[0]] for s in sims[:5]]
+# 		# Get first 5 matches
+# 		listingss = [listings['listing_url'].iloc[s[0]] for s in sims[:5]]
 
-		botResponse = f"Here are the listing details: {listingss}.".replace('[','').replace(']','')
+# 		botResponse = f"Here are the listing details: {listingss}.".replace('[','').replace(']','')
 		
-		dispatcher.utter_message(text=botResponse)
+# 		dispatcher.utter_message(text=botResponse)
 
-		return []
+# 		return []
 
-class ActionlistingsPics_Embedding(Action):
+# class ActionlistingsPics_Embedding(Action):
 
-	def name(self) -> Text:
-		return "action_listings_pics_embedding"
+# 	def name(self) -> Text:
+# 		return "action_listings_pics_embedding"
 
-	def run(self, dispatcher: CollectingDispatcher,
-			tracker: Tracker,
-			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+# 	def run(self, dispatcher: CollectingDispatcher,
+# 			tracker: Tracker,
+# 			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-		userMessage = tracker.latest_message['text']
+# 		userMessage = tracker.latest_message['text']
 
-		new_doc = preprocess_string(userMessage)
-		test_doc_vector = list_embedding_model.infer_vector(new_doc)
-		sims = list_embedding_model.docvecs.most_similar(positive = [test_doc_vector])		
+# 		new_doc = preprocess_string(userMessage)
+# 		test_doc_vector = list_embedding_model.infer_vector(new_doc)
+# 		sims = list_embedding_model.docvecs.most_similar(positive = [test_doc_vector])		
 		
-		listingss = [listings['picture_url'].iloc[s[0]] for s in sims[:1]]
+# 		listingss = [listings['picture_url'].iloc[s[0]] for s in sims[:1]]
 
-		str = ''
-		for lst in listingss:
-			str = lst
+# 		str = ''
+# 		for lst in listingss:
+# 			str = lst
 
-		botResponse = f"Your requirement seems to match with the following listing:"
+# 		botResponse = f"Your requirement seems to match with the following listing:"
 		
-		dispatcher.utter_message(text=botResponse)
-		dispatcher.utter_message(image=str)
+# 		dispatcher.utter_message(text=botResponse)
+# 		dispatcher.utter_message(image=str)
 
-		return []
+# 		return []
 
 class ActionlistingsBook(Action):
 
@@ -220,137 +220,137 @@ class ActionlistingsCancel(Action):
 
 		return []
 
-class ActionGetHelpLink_Embedding(Action):
+# class ActionGetHelpLink_Embedding(Action):
 
-    def __init__(self):
-        super(ActionGetHelpLink_Embedding, self).__init__()
-        self.faq_data = json.load(open(raw+"faq.json", "rt", encoding="utf-8"))
-        self.sentence_embedding_choose(sentence_transformer_select, pretrained_model)
-        self.standard_questions_encoder = np.load(embeddings+"embeddings/questions_embedding.npy")
-        self.standard_questions_encoder_len = np.load(embeddings+"embeddings/questions_embedding_len.npy")
-        print(self.standard_questions_encoder.shape)
+#     def __init__(self):
+#         super(ActionGetHelpLink_Embedding, self).__init__()
+#         self.faq_data = json.load(open(raw+"faq.json", "rt", encoding="utf-8"))
+#         self.sentence_embedding_choose(sentence_transformer_select, pretrained_model)
+#         self.standard_questions_encoder = np.load(embeddings+"embeddings/questions_embedding.npy")
+#         self.standard_questions_encoder_len = np.load(embeddings+"embeddings/questions_embedding_len.npy")
+#         print(self.standard_questions_encoder.shape)
 
-    def sentence_embedding_choose(self, sentence_transformer_select=True, pretrained_model='stsb-roberta-large'):
-        self.sentence_transformer_select = sentence_transformer_select
-        if sentence_transformer_select:
-            self.bc = SentenceTransformer(pretrained_model)
-        else:
-            self.bc = BertClient(check_version=False)
+#     def sentence_embedding_choose(self, sentence_transformer_select=True, pretrained_model='stsb-roberta-large'):
+#         self.sentence_transformer_select = sentence_transformer_select
+#         if sentence_transformer_select:
+#             self.bc = SentenceTransformer(pretrained_model)
+#         else:
+#             self.bc = BertClient(check_version=False)
 
-    def get_most_similar_standard_question_id(self, query_question):
-        if self.sentence_transformer_select:
-            query_vector = torch.tensor(self.bc.encode([query_question])[0]).numpy()
-        else:
-            query_vector = self.bc.encode([query_question])[0]
-        print("Question received at action engine")
-        score = np.sum((self.standard_questions_encoder * query_vector), axis=1) / (
-                self.standard_questions_encoder_len * (np.sum(query_vector * query_vector) ** 0.5))
-        top_id = np.argsort(score)[::-1][0]
-        return top_id, score[top_id]
+#     def get_most_similar_standard_question_id(self, query_question):
+#         if self.sentence_transformer_select:
+#             query_vector = torch.tensor(self.bc.encode([query_question])[0]).numpy()
+#         else:
+#             query_vector = self.bc.encode([query_question])[0]
+#         print("Question received at action engine")
+#         score = np.sum((self.standard_questions_encoder * query_vector), axis=1) / (
+#                 self.standard_questions_encoder_len * (np.sum(query_vector * query_vector) ** 0.5))
+#         top_id = np.argsort(score)[::-1][0]
+#         return top_id, score[top_id]
 
-    def name(self) -> Text:
-        return "action_get_HelpLink_Embedding"
+#     def name(self) -> Text:
+#         return "action_get_HelpLink_Embedding"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        query = tracker.latest_message['text']
-        print(query)
-        most_similar_id, score = self.get_most_similar_standard_question_id(query)
-        print("The question is matched with id:{} with score: {}".format(most_similar_id,score))
-        if float(score) > score_threshold: # This confidence scores can be adjusted based on your need!!
-            dispatcher.utter_message('Finding help, please wait!')
-            dispatcher.utter_message('Please refer the below help link for more informaion:')
-            response = self.faq_data[most_similar_id]['a']
-            dispatcher.utter_message(response)
-            dispatcher.utter_message("Did it help?")
-        else:
-            response = "Sorry, I don't see any matched help :("
-            dispatcher.utter_message(response)
-            dispatcher.utter_message("Sorry, I can't answer your question. You can dial customer support +1-844-234-2500")
-        return []
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#         query = tracker.latest_message['text']
+#         print(query)
+#         most_similar_id, score = self.get_most_similar_standard_question_id(query)
+#         print("The question is matched with id:{} with score: {}".format(most_similar_id,score))
+#         if float(score) > score_threshold: # This confidence scores can be adjusted based on your need!!
+#             dispatcher.utter_message('Finding help, please wait!')
+#             dispatcher.utter_message('Please refer the below help link for more informaion:')
+#             response = self.faq_data[most_similar_id]['a']
+#             dispatcher.utter_message(response)
+#             dispatcher.utter_message("Did it help?")
+#         else:
+#             response = "Sorry, I don't see any matched help :("
+#             dispatcher.utter_message(response)
+#             dispatcher.utter_message("Sorry, I can't answer your question. You can dial customer support +1-844-234-2500")
+#         return []
 
-class ActionlistingsReviews_Embedding(Action):
+# class ActionlistingsReviews_Embedding(Action):
 
-	def name(self) -> Text:
-		return "action_listings_reviews_Embedding"
+# 	def name(self) -> Text:
+# 		return "action_listings_reviews_Embedding"
 
-	def run(self, dispatcher: CollectingDispatcher,
-			tracker: Tracker,
-			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+# 	def run(self, dispatcher: CollectingDispatcher,
+# 			tracker: Tracker,
+# 			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-		userMessage = tracker.latest_message['text']
-		# use model to find the listings
-		new_doc = preprocess_string(userMessage)
-		test_doc_vector = review_embedding_model.infer_vector(new_doc)
-		sims = review_embedding_model.docvecs.most_similar(positive = [test_doc_vector])		
+# 		userMessage = tracker.latest_message['text']
+# 		# use model to find the listings
+# 		new_doc = preprocess_string(userMessage)
+# 		test_doc_vector = review_embedding_model.infer_vector(new_doc)
+# 		sims = review_embedding_model.docvecs.most_similar(positive = [test_doc_vector])		
 		
-		# Get first 5 matches
-		listingss=[]
-		for s in sims:
-			listingss.append('https://www.airbnb.com/rooms/'+str(s[0]))
+# 		# Get first 5 matches
+# 		listingss=[]
+# 		for s in sims:
+# 			listingss.append('https://www.airbnb.com/rooms/'+str(s[0]))
 
-		botResponse = f"Here are the top recommendation for you: {listingss}.".replace('[','').replace(']','')
+# 		botResponse = f"Here are the top recommendation for you: {listingss}.".replace('[','').replace(']','')
 				
-		dispatcher.utter_message(text=botResponse)
+# 		dispatcher.utter_message(text=botResponse)
 
-		return []
+# 		return []
 
-## Recommendations based on collaborative filtering.
-class ActionlistingsDetails_CBF(Action):
-	def name(self) -> Text:
-		return "action_listings_details_cbf"
+# ## Recommendations based on collaborative filtering.
+# class ActionlistingsDetails_CBF(Action):
+# 	def name(self) -> Text:
+# 		return "action_listings_details_cbf"
 
-	def run(self, dispatcher: CollectingDispatcher,
-			tracker: Tracker,
-			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+# 	def run(self, dispatcher: CollectingDispatcher,
+# 			tracker: Tracker,
+# 			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-		userMessage = tracker.latest_message['text']
-		userMessage='2818' ##(to-be-replaced) with dynamic value
-		# use model to find the listings
-		model = Word2Vec.load(offline_models+'ContentBasedFilter')		
-		sims = model.wv.similar_by_vector(userMessage, topK+1)[1:]
-		# Get first 5 matches
-		listingss=[]
-		for s in sims:
-			listingss.append('https://www.airbnb.com/rooms/'+s[0])
+# 		userMessage = tracker.latest_message['text']
+# 		userMessage='2818' ##(to-be-replaced) with dynamic value
+# 		# use model to find the listings
+# 		model = Word2Vec.load(offline_models+'ContentBasedFilter')		
+# 		sims = model.wv.similar_by_vector(userMessage, topK+1)[1:]
+# 		# Get first 5 matches
+# 		listingss=[]
+# 		for s in sims:
+# 			listingss.append('https://www.airbnb.com/rooms/'+s[0])
 
-		botResponse = f"Here are the top recommendation for you: {listingss}.".replace('[','').replace(']','')
+# 		botResponse = f"Here are the top recommendation for you: {listingss}.".replace('[','').replace(']','')
 		
-		dispatcher.utter_message(text=botResponse)
+# 		dispatcher.utter_message(text=botResponse)
 
-		return []
+# 		return []
 
 ## Recommendations based on collaborative filtering.
-class ActionlistingsDetails_ColabF(Action):
-	def name(self) -> Text:
-		return "action_listings_details_colabf"
+# class ActionlistingsDetails_ColabF(Action):
+# 	def name(self) -> Text:
+# 		return "action_listings_details_colabf"
 
-	def run(self, dispatcher: CollectingDispatcher,
-			tracker: Tracker,
-			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+# 	def run(self, dispatcher: CollectingDispatcher,
+# 			tracker: Tracker,
+# 			domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-		userMessage = tracker.latest_message['text']
+# 		userMessage = tracker.latest_message['text']
 	
-		# convert this into a dataframe so that it can be passed into the recommendForUserSubset
+# 		# convert this into a dataframe so that it can be passed into the recommendForUserSubset
 		
-		user_id = [[164729]] ##(to-be-replaced) with dynamic value
-		functiondf = sc.parallelize(user_id).toDF(['reviewer_id'])
+# 		user_id = [[164729]] ##(to-be-replaced) with dynamic value
+# 		functiondf = sc.parallelize(user_id).toDF(['reviewer_id'])
 
-		recommendations = alsmodel.recommendForUserSubset(functiondf , topK)
-		recommendations.collect()
+# 		recommendations = alsmodel.recommendForUserSubset(functiondf , topK)
+# 		recommendations.collect()
 
-		sims = [recommendations.collect()[0]['recommendations'][x]['listing_id'] for x in range(0,topK)]
-		# Get first 5 matches
-		listingss=[]
-		for s in sims:
-			listingss.append('https://www.airbnb.com/rooms/'+str(s))
+# 		sims = [recommendations.collect()[0]['recommendations'][x]['listing_id'] for x in range(0,topK)]
+# 		# Get first 5 matches
+# 		listingss=[]
+# 		for s in sims:
+# 			listingss.append('https://www.airbnb.com/rooms/'+str(s))
 
-		botResponse = f"Here are the top recommendation for you: {listingss}.".replace('[','').replace(']','')
+# 		botResponse = f"Here are the top recommendation for you: {listingss}.".replace('[','').replace(']','')
 		
-		dispatcher.utter_message(text=botResponse)
+# 		dispatcher.utter_message(text=botResponse)
 
-		return []
+# 		return []
 
 ## Recommendations based on real-time collaborative filtering.
 class ActionlistingsDetails_Neo4jColabF(Action):
